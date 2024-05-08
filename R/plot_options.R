773d6bd85be8299f074a094eee87ec28cf0ad334
luyckxn <nicolas.luyckx@calvagone.com>
2024-05-08 15:33:09 +0200

    #8 Plot options, code review.

--- R/plot_options.R
+++ R/plot_options.R
@@ -6,7 +6,8 @@
 #' 
 #' @slot ylim suggested Y-axis limit
 #' @slot timeref POSIXct time corresponding to TIME=0
-#' @slot timelabel label of datetime on X-axis
+#' @slot date_labels label of datetime on X-axis
+#' @slot date_breaks data breaks, e.g. '1 day'
 #' @slot minor_breaks_interval minor breaks interval in hours
 #' @export
 setClass(
@@ -14,8 +15,9 @@ setClass(
   representation(
     ylim="numeric",
     timeref="POSIXct",
-    timelabel="character",
-    minor_breaks_interval="integer"
+    date_labels="character",
+    date_breaks="character", # E.g. '1 day'
+    minor_breaks_interval="integer" # E.g. 6
   )
 )
 
@@ -23,17 +25,118 @@ setClass(
 #' 
 #' @param ylim suggested Y-axis limit
 #' @param timeref POSIXct time corresponding to TIME=0 in simulation
-#' @param timelabel label of datetime on X-axis
+#' @param date_labels label of datetime on X-axis
+#' @param date_breaks data breaks, e.g. '1 day'
 #' @param minor_breaks_interval minor breaks interval in hours
 #' @return an object
 #' @export
-PlotOptions <- function(ylim=NULL, timeref=NULL, timelabel="%b %d", minor_breaks_interval=6) {
+PlotOptions <- function(ylim=NULL, timeref=NULL, date_labels="%b %d", date_breaks="1 day", minor_breaks_interval=6) {
   if (is.null(ylim)) {
     ylim <- NA
   }
   if (is.null(timeref)) {
     timeref <- NA
   }
-  return(new("plot_options", ylim=as.numeric(ylim), timeref=as.POSIXct(timeref), timelabel=timelabel,
-             minor_breaks_interval=as.integer(minor_breaks_interval)))
+  return(new("plot_options", ylim=as.numeric(ylim), timeref=as.POSIXct(timeref), date_labels=date_labels,
+             date_breaks=date_breaks, minor_breaks_interval=as.integer(minor_breaks_interval)))
 }
+
+#_______________________________________________________________________________
+#----                           add                                   ----
+#_______________________________________________________________________________
+
+setMethod("add", signature = c("ANY", "plot_options"), definition = function(object, x, variable) {
+  options <- x
+  plot <- object
+  
+  # Use suggested Y limit
+  suggestedYLim <- options@ylim
+  
+  maxYValue <- maxValueInPlot(plot=plot, variable=variable)
+  
+  if (!is.na(suggestedYLim)) {
+    if (maxYValue > suggestedYLim) {
+      suggestedYLim <- maxYValue
+    }
+    plot <- plot + 
+      ggplot2::ylim(limits=c(0, suggestedYLim))
+  }
+  
+  timeref <- options@timeref
+  
+  # Datetime label
+  if (!is.na(timeref)) {
+    plot <- plotToPOSIXct(plot=plot, timeref=timeref)
+    
+    fun <- function(limits) {
+      return(minorBreaksCustom(limits=limits, breaksInterval=options@minor_breaks_interval))
+    }
+    plot <- plot +
+      ggplot2::scale_x_datetime(date_breaks=options@date_breaks, minor_breaks=fun, date_labels=options@date_labels)
+  }
+  
+  return(plot)
+})
+
+
+#' Custom minor breaks based on limits.
+#' 
+#' @param limits limits, vector of 2 POSIXct values
+#' @param ii minor breaks interval, in hours
+#' @importFrom lubridate dhours interval
+#' @export
+minorBreaksCustom <- function(limits, breaksInterval) {
+  initTime <- toDateTime(date=as.character(as.Date(limits[1])), time="00:00")
+  maxTime <- limits[2]
+  intervalDuration <- (lubridate::interval(initTime, maxTime) %>% as.numeric())/3600
+  noOfBreaks <- intervalDuration/breaksInterval
+  minor_breaks <- seq_len(noOfBreaks) %>%
+    purrr::map_dbl(~initTime + lubridate::dhours(.x*breaksInterval)) %>%
+    as.POSIXct()
+  return(minor_breaks)
+}
+
+#' Automatically convert all TIME columns found in plot to POSIXct given the time reference.
+#' 
+#' @param plot ggplot2 plot
+#' @param timeref time reference
+#' @return updated plot
+#' @export
+plotToPOSIXct <- function(plot, timeref) {
+  plot$data <- timeToPOSIXct(plot$data, timeref=timeref)
+  plot$layers <- plot$layers %>% purrr::map(.f=function(layer) {
+    layer$data <- timeToPOSIXct(layer$data, timeref=timeref)
+    return(layer)
+  })
+  
+  return(plot)
+}
+
+#' Automatically get the max value from the plot.
+#' 
+#' @param plot ggplot2 plot
+#' @return max value in all data layers
+#' @export
+maxValueInPlot <- function(plot, variable) {
+  maxValue1 <- max(plot$data[, variable])
+  maxValue2 <- plot$layers %>% purrr::map(.f=function(layer) {
+    if (variable %in% colnames(layer$data)) {
+      return(max(layer$data[, variable]))
+    } else {
+      return(NULL)
+    }
+    return(layer)
+  }) %>% purrr::discard(~is.null(.x)) %>%
+    purrr::flatten_dbl() %>%
+    max()
+  maxValue <- max(c(maxValue1, maxValue2))
+  return(maxValue)
+}
+
+timeToPOSIXct <- function(x, timeref) {
+  if ("TIME" %in% colnames(x)) {
+    x <- x %>%
+      dplyr::mutate(TIME=timeref + lubridate::dhours(TIME))
+  }
+  return(x)
+} 
--- R/quick_plot.R
+++ R/quick_plot.R
@@ -42,82 +42,21 @@ setMethod("quickPlot", signature("campsismap_model", "dataset", "numeric", "logi
     resultsPop <- predict(object=model, dataset=dataset, etas=rep(0, length(model@eta_names)))
   }
 
-  # Time reference
-  timeref <- options@timeref
-  
-  if (!is.na(timeref)) {
-    results <- results %>%
-      dplyr::mutate(TIME=timeref + lubridate::dhours(TIME))
-    if (pop) {
-      resultsPop <- resultsPop %>%
-        dplyr::mutate(TIME=timeref + lubridate::dhours(TIME))
-    }
-    if (nrow(dv) > 0) {
-      dv <- dv %>%
-        dplyr::mutate(TIME=timeref + lubridate::dhours(TIME))
-    }
-  }
-  
   plot <- ggplot2::ggplot(data=results, mapping=ggplot2::aes(x=TIME, y=.data[[model@variable]])) +
     ggplot2::geom_line(linewidth=1, alpha=0.6, color="#B90E1E")
-  
-  # Take max
-  maxYValue <- max(results[[model@variable]])
-  
+
   if (pop) {
     plot <- plot +
       ggplot2::geom_line(data=resultsPop, linewidth=1, alpha=0.6, color="#6196B4")
-    
-    # Update max value
-    maxYValuePop <- max(resultsPop[[model@variable]])
-    if (maxYValue < maxYValuePop) {
-      maxYValue <- maxYValuePop
-    }
   }
   
   if (nrow(dv) > 0) {
     plot <- plot +
       ggplot2::geom_point(mapping=ggplot2::aes(x=TIME, y=DV, group=NULL), data=dv, color="black")
-    
-    # Update max value
-    maxYValueDV <- max(dv$DV)
-    if (maxYValue < maxYValueDV) {
-      maxYValue <- maxYValueDV
-    }
   }
 
-  # Use suggested Y limit
-  suggestedYLim <- options@ylim
-  
-  if (!is.na(suggestedYLim)) {
-    if (maxYValue > suggestedYLim) {
-      suggestedYLim <- maxYValue
-    }
-    plot <- plot + 
-      ggplot2::ylim(limits=c(0, suggestedYLim))
-  }
-  
-  # Datetime label
-  if (!is.na(timeref)) {
-    timelabel <- options@timelabel
-    breaksInterval <- options@minor_breaks_interval
-
-    # Set locale to English
-    invisible(Sys.setlocale("LC_ALL", "English.utf8"))
-    
-    initTime <- toDateTime(date=as.character(as.Date(timeref)), time="00:00")
-    maxTime <- timeref + lubridate::dhours(max(dataset %>% getSimulationTimes()))
-    intervalDuration <- (lubridate::interval(initTime, maxTime) %>% as.numeric())/3600
-    noOfBreaks <- intervalDuration/breaksInterval
-    minor_breaks <- seq_len(noOfBreaks) %>%
-      purrr::map_dbl(~initTime + lubridate::dhours(.x*breaksInterval)) %>%
-      as.POSIXct()
-
-    plot <- plot +
-      ggplot2::scale_x_datetime(date_breaks="1 day", minor_breaks=minor_breaks, date_labels=timelabel)
-  }
+  # Add options
+  plot <- plot %>% add(options, variable=model@variable)
 
   return(plot + ggplot2::theme_bw())
 })
-
-
