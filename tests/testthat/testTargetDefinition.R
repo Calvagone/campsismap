4789d2fb046b865bdad8d1c92c56f0c75c195026
luyckxn <nicolas.luyckx@calvagone.com>
2024-07-11 13:37:29 +0200

    #10 Method importUsecase in tests.

--- tests/testthat/testTargetDefinition.R
+++ tests/testthat/testTargetDefinition.R
@@ -10,18 +10,34 @@ getUsecaseFile <- function(usecase, file) {
   return(file.path(testFolder, "usecases", usecase, file))
 }
 
-test_that(getTestName("Target definition per window can be converted to target definition per dose"), {
-  
-  usecase <- "usecase1"
-  
+importUsecase <- function(usecase) {
   dateTime0 <- read.datetimecsv(getUsecaseFile(usecase, "dosing.csv"))$Datetime[1]
   dosing <- read.datetimecsv(getUsecaseFile(usecase, "dosing.csv"), relative=TRUE, dateTime0=dateTime0)
   obs <- read.datetimecsv(getUsecaseFile(usecase, "observations.csv"), relative=TRUE, dateTime0=dateTime0)
   target <- read.datetimecsv(getUsecaseFile(usecase, "target.csv"), relative=TRUE, dateTime0=dateTime0)
+  return(list(dateTime0=dateTime0, dosing=dosing, obs=obs, target=target))
+}
+
+test_that(getTestName("Target definition per window can be converted to target definition per dose"), {
+  
+  usecase <- "usecase1"
+  data <- importUsecase(usecase)
+  target <- data$target
+  dosing <- data$dosing
   
   targetWindow <- TargetDefinitionPerWindow(target %>% rename(VALUE=Target))
   targetDose <- targetWindow %>% export(TargetDefinitionPerDose(), dosing=dosing)
   
   expect_equal(length(targetDose), nrow(dosing))
   expect_equal(targetDose, TargetDefinitionPerDose(tibble(DOSENO=1:8, VALUE=c(50,50,60,60,70,70,70,70))))
+})
+
+test_that(getTestName("Target definition per window can be converted to an effective target definition for campsismap"), {
+  
+  usecase <- "usecase1"
+  data <- importUsecase(usecase)
+  target <- data$target
+  dosing <- data$dosing
+  
+  
 })
